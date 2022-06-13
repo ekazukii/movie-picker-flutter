fbc623fd779dfb117f97eacf553ad5ccfbb31063
Baptiste Loison <baptisteloison8400@gmail.com>
2022-06-13 12:06:03 +0200

    Add provider picker in movie picker page

--- lib/picker.dart
+++ lib/picker.dart
@@ -4,9 +4,10 @@ import 'package:flutter/material.dart';
 //import 'package:shared_preferences/shared_preferences.dart';
 
 import './widgets/typePicker.dart';
+import './widgets/providersPicker.dart';
 
 class Picker extends StatefulWidget {
-  const Picker({ Key? key }) : super(key: key);
+  const Picker({Key? key}) : super(key: key);
 
   @override
   State<Picker> createState() => _Picker();
@@ -15,28 +16,34 @@ class Picker extends StatefulWidget {
 class _Picker extends State<Picker> {
   RangeValues _currentRangeValues = const RangeValues(30, 60);
   HashSet<String> _typesSelected = HashSet<String>();
+  HashSet<int> _providersSelected = HashSet<int>();
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         RangeSlider(
-          onChanged: (RangeValues newValues) {
-            setState(() {
-              _currentRangeValues = newValues;
-            });
-          },
-          divisions: 300,
-          labels: RangeLabels(
-            _currentRangeValues.start.round().toString(),
-            _currentRangeValues.end.round().toString(),
-          ),
-          max: 300,
-          min: 0,
-          values: _currentRangeValues
-        ), TypePicker(onChanged: (HashSet<String> vals) {
+            onChanged: (RangeValues newValues) {
+              setState(() {
+                _currentRangeValues = newValues;
+              });
+            },
+            divisions: 300,
+            labels: RangeLabels(
+              _currentRangeValues.start.round().toString(),
+              _currentRangeValues.end.round().toString(),
+            ),
+            max: 300,
+            min: 0,
+            values: _currentRangeValues),
+        TypePicker(onChanged: (HashSet<String> vals) {
           setState(() {
             _typesSelected = vals;
+          });
+        }),
+        ProvidersPicker(onChanged: (HashSet<int> vals) {
+          setState(() {
+            _providersSelected = vals;
           });
         }),
       ],
